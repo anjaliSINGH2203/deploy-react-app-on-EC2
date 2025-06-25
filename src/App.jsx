6e098fbe7fb6754ae3f7741c5b970ed4880d24a5
Anjali Singh <anjalisinghthakur222@gmail.com>
2025-06-25 10:20:09 +0530

    first code

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>hello world</h1>
+    <h2>my first react app on AWS EC2</h2>
+    <h3>deployed by Anjali Singh</h3>
+    </div>
+  )
+}
+
+export default App
